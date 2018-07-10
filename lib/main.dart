c5b2ba60bef33e5280f2cce796019633b0507562
Oleg Markoff <mawglih.com>
2018-07-09 22:57:07 -0400

    initial

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+import './pages/admin.dart';
+import './pages/product.dart';
+import './pages/products.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+    State<StatefulWidget> createState() {
+      return _MyAppState();
+    }
+}
+class _MyAppState extends State<MyApp> {
+  List<Map<String,String>> _products = [];
+  void _deleteProduct(int index) {
+      setState(() {
+              _products.removeAt(index);
+            });
+    }
+  void _addProduct(Map<String, String> product) {
+    setState(() {
+          _products.add(product);
+        });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        accentColor: Colors.pinkAccent
+      ),
+      // home: AuthPage(),
+      routes: {
+        '/': (BuildContext context) => ProductsPage(_products, _addProduct, _deleteProduct),
+        '/admin': (BuildContext context) => AdminPage(),
+      },
+      onGenerateRoute: (RouteSettings settings) {
+        final List<String> pathElements = settings.name.split('/');
+        if (pathElements[0] != '') {
+          return null;
+        }
+        if(pathElements[1]== 'product') {
+          final int index = int.parse(pathElements[2]);
+          return MaterialPageRoute<bool>(
+                              builder: (BuildContext context) => ProductPage(
+                                _products[index]['title'], _products[index]['image']),
+                            );
+        }
+        return null;
+      
+      },
+      onUnknownRoute: (RouteSettings settings) {
+        return MaterialPageRoute(
+          builder: (BuildContext context) =>
+            ProductsPage(_products, _addProduct, _deleteProduct));
+          },
+        );
+      }
+}
---
+++ lib/products.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+
+class Products extends StatelessWidget {
+  final List<Map<String, String>> products;
+  final Function deleteProduct;
+  Products(this.products, {this.deleteProduct});
+  Widget _buildProductItem(BuildContext context, int index) {
+    return Card(
+                child: Column(
+                  children: <Widget>[
+                    Image.asset(products[index]['image']),
+                    Text(products[index]['title']),
+                    ButtonBar(
+                      alignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        FlatButton(
+                          child: Text('Details'), 
+                          onPressed: () => Navigator.pushNamed<bool>(
+                            context, '/product/' + index.toString())
+                            .then((bool value) {
+                            if(value) {
+                              deleteProduct(index);
+                            }
+                          }), 
+                        )
+                      ],
+                      ),
+                      ]
+                )
+              );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return products.length > 0 ? ListView.builder(
+            itemBuilder: _buildProductItem,
+            itemCount: products.length,
+            ) : Center( 
+              child: Text(
+                "Please add products"),
+                );
+  }
+}
